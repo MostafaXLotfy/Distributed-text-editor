9b1465f3b8868c5ce9d598127d483a79a4fde8cf
zyad yasser <zyadyasser626@gmail.com>
2022-05-12 16:57:28 +0200

    added server side OT logic

--- server/app.js
+++ server/app.js
@@ -1,11 +1,38 @@
 const express = require("express")
 const app = express()
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 const path = require("path")
+const Delta = require("quill-delta")
+
+let delta = new Delta()
+let current_v = 0
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on("document edit", (edit)=>{
+        if (edit.v > current_v) {
+            delta.compose(edit.delta)
+        }
+        else{
+            delta.transform(edit.delta, true)
+        }
+        current_v++
+        socket.broadcast.emit("document edit", {
+            "delta":delta,
+            "v":current_v
+        });
+    })
+    //Whenever someone disconnects this piece of code executed
+    socket.on('disconnect', function () {
+    console.log('A user disconnected');
+ });
+  });
 
 app.get("/",(req, res)=>{
     res.sendFile(path.join(__dirname, "../clientApp/index.html"))
 })
 
-app.listen(3000, ()=>{
+http.listen(3000, ()=>{
     console.log("app listening on port 3000")
 })
