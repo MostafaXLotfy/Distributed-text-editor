f01a292207a8e2694e78a6e29a3932300e979b94
Mostafa Lotfy <mostafaXlotfy@gmail.com>
2022-05-16 01:30:47 +0200

    refactored client app and added an intial version of pseudo ot

--- clientApp/client_app.js
+++ clientApp/client_app.js
@@ -2,60 +2,60 @@
 let quill = new Quill('#editor', {
   theme: 'snow'
 });
+
+
 let Delta = Quill.import('delta')
-var socket = io()
-let change = new Delta()
-
-
-let acknowledged = true
-let sent = false
-var old_doc  = null
-let current_version = 0
+let socket = io()
+var client_state = null
 let number_of_users = 0
-let diff = null
 //TODO:: make an object that contains the state of the clinet
 
+
 const on_text_change = (delta) =>{
-    new_change = change.compose(delta);
-    diff = change.diff(new_change)
-    current_version++;
-    change = new_change
-    if (acknowledged){
+  diff = client_state.update_document(delta)
+  // TODO:: Add logic for pending edits
+  if (!client_state.waiting_ack){
       socket.emit("document edit", {
-        "delta":delta,
-        v: current_version
+        "delta":diff,
+        "v": client_state.current_version
       })
+      client_state.waiting_ack = true
+    }else{
+      client_state.push_pending_delta(diff, client_state.current_version)
     }
-    sent = true
-    acknowledged = false
 }
 
 
 socket.on("document broadcast", (incoming_document)=>{
-  current_version = incoming_document.v
   console.log(`recevied a new document:\n ${incoming_document.delta}`)
-  if (sent && _.isEqual(diff.ops, incoming_document.delta.ops)){
-    sent = false
-    acknowledged = true
+  if (client_state.waiting_ack && _.isEqual(diff.ops, incoming_document.delta.ops)){
+    client_state.waiting_ack = false
+  }else if(client_state.waiting_ack && client_state.is_pending_delta){
+    socket.emit("document edit", client_state.pop_pending_delta())
+    quill.setContents(client_state.current_document, "silent")
   }else{
-    change = change.compose(incoming_document.delta)
-    quill.setContents(change, "silent")
+    client_state.update_document(incoming_document.delta, incoming_document.v)
+    quill.setContents(client_state.current_document, "silent")
   }
 }) 
 
-socket.on("latest edits", (incoming_document)=>{
-  current_version = incoming_document.v
-  console.log(current_version)
-  console.log(`recevied latest edits:\n ${incoming_document.delta}`)
-  if (sent === true){
-    //TODO:: this will go wrong fix it
-    if(incoming_document.delta === sent_delta){
-      sent = false
-      acknowledged = true
-    }
+socket.on("latest edits", (new_document)=>{
+
+  if (!client_state){
+    client_state = new ClientState(new_document.delta, new_document.v)
+  }else{
+    client_state.update_document(new_document.delta, new_document.v)
   }
-  change = change.compose(incoming_document.delta)
-  quill.setContents(change, "silent")
+  console.log(`recevied latest edits:\n`)
+  //TODO::doen't remember what this do 
+  // if (sent === true){
+  //   //TODO:: this will go wrong fix it
+  //   if(incoming_document.delta === sent_delta){
+  //     sent = false
+  //     acknowledged = true
+  //   }
+  // }
+  quill.setContents(client_state.current_document, "silent")
 })
 
 socket.on("user connected", (live_users_counter)=>{
@@ -68,4 +68,12 @@ socket.on("user disconnected", (live_users_counter)=>{
   number_of_users = live_users_counter
 })
 
+// socket.on("connect", ()=>{
+//   let client_state = null
+// })
 quill.on('text-change', on_text_change)
+
+
+function main(){
+
+}
---
+++ clientApp/client_state.js
@@ -0,0 +1,29 @@
+// let Delta = Quill.import('delta')
+class ClientState{
+    constructor(current_document, current_version){
+        this.waiting_ack = false
+        this.current_document = new Delta().compose(current_document)
+        this.current_version = current_version
+        this.pending_edits = []
+    }
+
+    update_document(delta, version=null){
+        let new_document = this.current_document.compose(delta)
+        let diff = this.current_document.diff(new_document)
+        this.current_document = new_document
+        this.current_version = (version === null ? this.current_version + 1 : version)
+        return diff
+    }
+
+    push_pending_delta(delta){
+        this.pending_edits.push({"delta":delta, "v":this.current_version})
+    }
+
+    pop_pending_delta(){
+        return this.pending_edits.shift()        
+    }
+
+    is_pending_delta(){
+        return this.pending_edits.length === 0
+    }
+}
